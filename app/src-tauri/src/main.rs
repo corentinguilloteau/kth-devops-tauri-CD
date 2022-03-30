62eb23d8af2d2b490e90a5c49114b37a319e80c7
Guilloteau Corentin <corentin.guilloteau@telecom-paris.fr>
2022-03-30 19:04:30 +0200

    Added version arg support

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,10 +1,25 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use clap::{arg, Command};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let context = tauri::generate_context!();
+
+    let cmd = Command::new("KTH DevOps Demo")
+        .disable_version_flag(true)
+        .version(format! {"v{}", context.package_info().version}.as_str())
+        .author("Corentin Guilloteau")
+        .arg(arg!(-v - -version))
+        .get_matches();
+
+    if cmd.is_present("version") {
+        println!("{}", context.package_info().version.as_str());
+    } else {
+        let _app = tauri::Builder::default()
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    }
 }
