ce19ce85d9632ae25d14e4d0ee4dd5249871279c
Guilloteau Corentin <corentin.guilloteau@telecom-paris.fr>
2022-04-01 18:56:10 +0200

    Added route to upload new udpateé

--- update_server/database/db.js
+++ update_server/database/db.js
@@ -2,10 +2,18 @@ const sqlite3 = require("sqlite3").verbose();
 
 const updateDatabase = new sqlite3.Database(path.resolve("./stores/updates.db"), { fileMustExist: true });
 
-function getLatestUpdate(architecture, version) {
+function getLatestUpdate(target) {
 	const data = updateDatabase
 		.prepare(`SELECT * FROM UPDATES WHERE target = ? ORDER BY ID DESC LIMIT 1`)
-		.params([version]);
+		.params([target]);
 
 	return { url: data.url, signature: data.signature, version: data.version };
 }
+
+function addNewVersion(target, version, url, signature) {
+	updateDatabase
+		.prepare(`INSERT INTO UPDATES (target, version, url, signature) VALUES (?, ?, ?, ?)`)
+		.params([target, version, url, signature]);
+}
+
+export { getLatestUpdate, addNewVersion };
--- update_server/routes/kthdevops/tauriupdaterdemo/index.js
+++ update_server/routes/kthdevops/tauriupdaterdemo/index.js
@@ -5,11 +5,11 @@ const { check } = require("express-validator");
 
 const targets = ["windows", "linux", "macos"];
 
-/* GET home page. */
+/* GET latest update. */
 router.get(
 	"/:target/:version",
 	check("target").exists().isString().escape().if(!targets.includes(target)),
-	check("target")
+	check("version")
 		.exists()
 		.isString()
 		.escape()
@@ -22,6 +22,39 @@ router.get(
 			res.json(data);
 		} else {
 			res.sendStatus(204);
+		}
+	}
+);
+
+/* POST latest update. */
+router.post(
+	"/",
+	check("target").exists().isString().escape().if(!targets.includes(target)).escape(),
+	check("version")
+		.exists()
+		.isString()
+		.escape()
+		.matches(/^[0-9]+\.[0-9]+\.[0-9]+$/)
+		.escape(),
+	check("url")
+		.exists()
+		.isString()
+		.isURL({
+			protocols: ["https"],
+			require_protocol: true,
+			require_port: false,
+			require_valid_protocol: true,
+		})
+		.escape(),
+	check("signature").exists().isBase64().escape(),
+	check("auth").exists().isString().escape(),
+	function (req, res, next) {
+		if (process.env.AUTH_SECRET && req.params.auth === process.env.AUTH_SECRET) {
+			db.addNewVersion(req.params.target, req.params.version, req.params.url, req.params.signature);
+
+			res.sendStatus(200);
+		} else {
+			res.sendStatus(403);
 		}
 	}
 );
