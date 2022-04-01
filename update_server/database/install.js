1d515facab2299e08a002f78857ba3b84688b469
Guilloteau Corentin <corentin.guilloteau@telecom-paris.fr>
2022-04-01 21:16:45 +0200

    Removed not null constraint on id

--- update_server/database/install.js
+++ update_server/database/install.js
@@ -4,7 +4,7 @@ async function installDB() {
 	const db = await newDatabaseConnection();
 
 	await db.run(
-		"CREATE TABLE IF NOT EXISTS UPDATES (id int NOT NULL, target TEXT, version TEXT, url TEXT, signature TEXT, PRIMARY KEY (id))"
+		"CREATE TABLE IF NOT EXISTS UPDATES (id int, target TEXT, version TEXT, url TEXT, signature TEXT, PRIMARY KEY (id))"
 	);
 }
 
